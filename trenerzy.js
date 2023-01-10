a4cbaa4e508304dde1a3a951d21229895a7edce0
maciejdylewicz <maciejdylewicz@gmail.com>
2023-01-10 13:27:06 +0100

    Update trenerzy.js

--- trenerzy.js
+++ trenerzy.js
@@ -69,3 +69,18 @@ function deleteRecord(r) {
     document.getElementById("table-trenerzy").deleteRow(i);
 }
 
+function displayTrenerzy(xml) {
+  var xmlDoc = xml.responseXML;
+  var table = "<tr><th>Id Trenera</th><th>Imię Trenera</th><th>Nazwisko Trenera</th></tr>";
+  var x = xmlDoc.getElementsByTagName("trener");
+  for (var i = 0; i < x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("id_trenera")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Imię_trenera")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("Nazwisko_trenera")[0].childNodes[0].nodeValue +
+    "</td><td><button onclick='deleteRecord(this)'>&times;</button></td></tr>";
+  }
+  document.getElementById("table-trenerzy").innerHTML = table;
+}
