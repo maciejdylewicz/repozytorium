76de4cad6deb66194f1f8d7b7848b7a396a68bad
maciejdylewicz <maciejdylewicz@gmail.com>
2023-01-10 13:04:26 +0100

    Create trenerzy.js

---
+++ trenerzy.js
@@ -0,0 +1,27 @@
+function loadTrenerzy() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      displayTrenerzy(this);
+    }
+  };
+  xhttp.open
+xhttp.open("GET", "trenerzy.xml", true);
+xhttp.send();
+}
+
+function displayTrenerzy(xml) {
+var xmlDoc = xml.responseXML;
+var table = "<tr><th>Id Trenera</th><th>Imię Trenera</th><th>Nazwisko Trenera</th></tr>";
+var x = xmlDoc.getElementsByTagName("trener");
+for (var i = 0; i < x.length; i++) {
+table += "<tr><td>" +
+x[i].getElementsByTagName("id_trenera")[0].childNodes[0].nodeValue +
+"</td><td>" +
+x[i].getElementsByTagName("Imię_trenera")[0].childNodes[0].nodeValue +
+"</td><td>" +
+x[i].getElementsByTagName("Nazwisko_trenera")[0].childNodes[0].nodeValue +
+"</td></tr>";
+}
+document.getElementById("table-trenerzy").innerHTML = table;
+}
