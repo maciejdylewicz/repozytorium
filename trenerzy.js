4b504085010c0d50be757560e273b25b29210f05
maciejdylewicz <maciejdylewicz@gmail.com>
2023-01-10 13:08:30 +0100

    Update trenerzy.js

--- trenerzy.js
+++ trenerzy.js
@@ -25,3 +25,27 @@ x[i].getElementsByTagName("Nazwisko_trenera")[0].childNodes[0].nodeValue +
 }
 document.getElementById("table-trenerzy").innerHTML = table;
 }
+
+
+function sortTable(column) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table-trenerzy");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[column];
+      y = rows[i + 1].getElementsByTagName("TD")[column];
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
